93c67d4d8e1f127b277ae59f9f1a33c6140e26d3
Shirish Pathania <shirishpathania@gmail.com>
2024-05-21 22:43:20 +1200

    Add project files.

---
+++ Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    // Child Class
+    public class Child
+    {
+        public int add(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+
+        }
+
+        public int sub(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CSharpBasics
+{
+    // Parent Class
+    internal class Program
+    {
+
+        // Your project will always begin execution from Main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // This line prints "Hello, World!" text in console window
+
+            // Initialise an object
+            Child childObj = new Child();
+
+            // Use object to call method of another class
+            int addResult = childObj.add(10, 20);
+            Console.WriteLine(addResult);
+            int subResult = childObj.sub(100, 50);
+            Console.WriteLine(subResult);
+        }
+
+        // Declaring and initialising variables at class level, can be used inside any method of the same class
+        String name = "James";
+        int age = 50;
+        Boolean isTester = true;
+
+        // User defined method with no return type
+        public void noReturnType()
+        {
+            // Declaring and initialising variables at method level, can only be accessed inside the method
+            int result = 10 + 20;
+            Console.WriteLine(result);
+        }
+
+        // User defined method with return type
+        public int withReturnType()
+        {
+            int result = 10 + 20;
+            Console.WriteLine(result);
+            return result;
+        }
+
+
+    }
+}
